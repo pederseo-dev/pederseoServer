38184115856d0d559f08b74e9d035d774c4b6c7d
pederseo-dev <olaf.acount@gmail.com>
2025-07-19 21:57:42 -0300

    test server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const MagicDef = require('magicdef');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const md = new MagicDef();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+function test() {
+    return "hola desde el server";
+}
+
+md.export(test);
+
+// Start server
+app.listen(PORT, () => {
+});
+
+module.exports = app; 
