2ee7c1307e01526b6766fe7189c35ea2cd2d38cb
Olaf <pederseo.dev@gmail.com>
2025-08-01 10:47:54 -0300

    basic server configurations

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const MagicDef = require('magicdef');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const md = new MagicDef();
-md.connect('sala-de-pruebas')
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-function test() {
-    return "hola desde el server";
-}
-
-md.export(test);
-
-// Start server
-app.listen(PORT, () => {
+// Ruta básica
+app.get('/', (req, res) => {
+  res.send('server para probar boludeces');
 });
 
-module.exports = app; 
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+module.exports = app;
